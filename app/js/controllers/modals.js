e1ee0766dfc5010775c708eb670d9aa8e7420765
Vaclav Klecanda <vencax77@gmail.com>
2015-02-25 08:24:30 +0100

    tree in link editor modal

--- app/js/controllers/modals.js
+++ app/js/controllers/modals.js
@@ -1,22 +1,37 @@
 
 angular.module("app")
 
-.controller('LinkSelectionCtrl', function($scope, $modalInstance, GithubSrvc, prefix) {
+.controller('LinkSelectionCtrl', function($scope, $q, $modalInstance, GithubSrvc, setup) {
 
+  $scope.setup = setup;
   $scope.subpages = [];
   $scope.posts = [];
   $scope.info = {title: '', link: ''};
   $scope.OKdisabled = true;
 
-  GithubSrvc.listPages('', function(err, pages) {
+  GithubSrvc.listPages('', function(err, rootpages) {
+
+    function _loadNode(n) {
+      GithubSrvc.listPages(n.path, function(err, pages) {
+        $scope.$apply(function() {
+          if(err || pages.length === 0) { n.nodes = []; return; }
+          pages.forEach(function(p) { _loadNode(p); });
+          n.nodes = pages;
+        });
+      });
+    }
+
+    rootpages.forEach(function(p) { _loadNode(p); });
     $scope.$apply(function() {
-      $scope.subpages = pages;
+      $scope.pages = [{
+        name: '/', path: '', nodes: rootpages
+      }];
     });
   });
 
   $scope.ok = function () {
     if($scope.OKdisabled) { return; }
-    var result = prefix + '[' + $scope.info.title + '](' + $scope.info.link + ')';
+    var result = $scope.setup.prefix + '[' + $scope.info.title + '](' + $scope.info.link + ')';
     $modalInstance.close(result);
   };
 
@@ -34,5 +49,50 @@ angular.module("app")
   $scope.$watch('info.link', function(newValue, oldValue) {
     $scope.OKdisabled = ! formOK();
   });
+
+  var protos = [
+    {name: 'http://', path: 'http://'},
+    {name: 'https://', path: 'https://'},
+  ];
+
+  $scope.isCollapsed = function(scope) {
+    return $scope.setup.path.indexOf(scope.$modelValue.path) !== 0;
+  };
+
+  $scope.dblclick = function(nodescope) {
+    var p = nodescope.$modelValue.path;
+    var idx = p.indexOf($scope.setup.path);
+    if(idx === 0) {  // relative link
+      p = p.substring($scope.setup.path.length+1, p.length);
+    } else {  // absolute path
+      p = '/' + p;
+    }
+    $scope.info.link = p + '/';
+  };
+
+  $scope.loadOptions = function(query) {
+    var deferred = $q.defer();
+
+    var inputPath = query.split('/');
+    inputPath.pop();
+    inputPath = inputPath.join('/');
+
+    var p;
+
+    if(query.indexOf('/') === 0) {  // load site pages
+      p = inputPath;
+    } else {  // load subpages
+      p = $scope.setup.path + inputPath;
+    }
+
+    GithubSrvc.listPages(p, function(err, items) {
+      items.push(protos[0]);
+      items.push(protos[1]);
+      deferred.resolve(items);
+    });
+
+    return deferred.promise;
+  };
+
 
 });
--- app/js/services/editor.js
+++ app/js/services/editor.js
@@ -3,13 +3,15 @@ angular.module("app")
 
 .factory('EditorSrvc', function($http, $modal) {
 
-  function _openDialog(prefix, done) {
+  function _openDialog(prefix, path, done) {
 
     var modalInstance = $modal.open({
       templateUrl: 'modal.linkselect.html',
       controller: 'LinkSelectionCtrl',
       resolve: {
-        prefix: function() { return prefix; }
+        setup: function() {
+          return { prefix: prefix, path: path };
+        }
       }
     });
 
@@ -27,16 +29,16 @@ angular.module("app")
 
   return {
 
-    onContentChange: function(content, onChanged) {
+    onContentChange: function(content, path, onChanged) {
       var idx = content.indexOf('![]()');
       if(idx >= 0) {
-        return _openDialog('!', function(link) {
+        return _openDialog('!', path, function(link) {
           return onChanged(content.replace('![]()', link));
         });
       }
       idx = content.indexOf('[]()');
       if(idx >= 0) {
-        _openDialog('', function(link) {
+        _openDialog('', path, function(link) {
           return onChanged(content.replace('[]()', link));
         });
       }
